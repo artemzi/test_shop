02c19e9d834dd3c8c9be3fba27ae3b28f0e083c1
Artem Zinoviev <arzinoviev@gmail.com>
2013-05-24 11:23:24 +0300

    some garbage

--- products/models.py
+++ products/models.py
@@ -6,6 +6,9 @@ class Products(models.Model):
     slug = models.SlugField(unique=True)
     description = models.CharField(max_length=500, blank=True, null=True)
     image1 = models.ImageField(upload_to='product_images', blank=True, null=True)
+    price = models.IntegerField(default=0.00)
+    sku = models.CharField(max_length=160, default=0)
+    inventory = models.IntegerField(default=0)
 
     def __unicode__(self):
         return self.name
